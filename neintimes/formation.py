3c8136a444de589a7c8b619d8c1e4656ec55155c
Alex <alb0026@auburn.edu>
2014-07-09 19:53:49 -0500

    formation data structure

---
+++ neintimes/formation.py
@@ -0,0 +1,18 @@
+#formation.py - definition of formation structure and helpers
+
+from vector import *
+
+class FormationSlot:
+	def __init__(self, spatialOffset=None, angularOffset=0, weapon=None, timeOffset=0 ):
+		zero = Vector2D(0,0)
+		if spatialOffset == None:
+			spatialOffset = zero
+		self.spatialOffset = spatialOffset
+		self.timeOffset = timeOffset
+		self.angularOffset = angularOffset
+		self.weapon = weapon
+
+class Formation:
+	def __init__(self, slotList):
+		for s in slotList:
+			self.slotList.append(s)
